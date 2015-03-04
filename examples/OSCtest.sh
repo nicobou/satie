5178c40ce8c9bac05a4fa4d769f8eb1dc54632bf
zack <zkfax@sympatico.ca>
2015-03-04 15:36:11 +0530

    updated osc message handling

--- examples/OSCtest.sh
+++ examples/OSCtest.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-
-oscsend localhost 18032 /spatosc/core sss createSource boo pink
-sleep 5
-oscsend localhost 18032 /spatosc/core sssf setProperty boo azi -0.7
-oscsend localhost 18032 /spatosc/core sssf setProperty boo volume 0.5
-sleep 3
-oscsend localhost 18032 /spatosc/core sss createSource dust dust
-oscsend localhost 18032 /spatosc/core sssf setProperty boo vol 0.1
+oscsend localhost 18032 /spatosc/core ss createSource sheefa
 sleep 1
-oscsend localhost 18032 /spatosc/core sssf setProperty dust azi 0.9
+oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf vol .1
+sleep 1
+oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf azi -1
+sleep 1
+oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf azi 1
+sleep 1
+oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf azi 0
+sleep 1
+oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf vol 1
 sleep 2
-oscsend localhost 18032 /spatosc/core sssf setProperty dust density1 20
-oscsend localhost 18032 /spatosc/core sssf setProperty dust density2 400
-sleep 3
-oscsend localhost 18032 /spatosc/core ss deleteNode boo
+oscsend localhost 18032 /spatosc/core/source/sheefa/uri s plugin://pink
+sleep 5
+oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf sfreq 1000
 sleep 1
-oscsend localhost 18032 /spatosc/core ss deleteNode dust
+oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf sfreq 500
+sleep 1
+oscsend localhost 18032 /spatosc/core/source/sheefa/uri s plugin://dust
+sleep 1
+oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf density1 20
+sleep 3
+oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf density2 400
+sleep 5
+oscsend localhost 18032 /spatosc/core ss deleteNode sheefa
