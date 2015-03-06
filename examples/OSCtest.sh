de7cd37a14858a2aadb159e94677da6f7742d079
zack <zkfax@sympatico.ca>
2015-03-06 10:57:19 +0530

    finished  state event setURI and prop hooks

--- examples/OSCtest.sh
+++ examples/OSCtest.sh
@@ -2,11 +2,19 @@
 
 # will create a node using the "default" plugin
 oscsend localhost 18032 /spatosc/core ss createSource sheefa
-sleep 2
-oscsend localhost 18032 /spatosc/core/source/sheefa/event sff noteOn 3 20
-sleep 3
-oscsend localhost 18032 /spatosc/core/source/sheefa/event sff noteOn 10 20
 sleep 1
+oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf amp .5
+sleep 1
+oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf amp 1
+sleep 1
+oscsend localhost 18032 /spatosc/core/source/sheefa/state f 0
+sleep 1
+oscsend localhost 18032 /spatosc/core/source/sheefa/state f 1
+sleep 1
+oscsend localhost 18032 /spatosc/core/source/sheefa/event sff noteOn 1 60
+sleep 1
+oscsend localhost 18032 /spatosc/core/source/sheefa/event sff noteOn 5 76
+sleep 2
 oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf vol .1
 sleep 1
 oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf azi -1
@@ -18,7 +26,7 @@ sleep 1
 oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf vol 1
 sleep 2
 oscsend localhost 18032 /spatosc/core/source/sheefa/uri s plugin://pink
-sleep 5
+sleep 3
 oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf sfreq 1000
 sleep 1
 oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf sfreq 500
@@ -28,5 +36,5 @@ sleep 1
 oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf density1 20
 sleep 3
 oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf density2 400
-sleep 5
+sleep 3
 oscsend localhost 18032 /spatosc/core ss deleteNode sheefa
