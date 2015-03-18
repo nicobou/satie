de222691d017561a747ff78e52478ddb1969c314
zack <zkfax@sympatico.ca>
2015-03-18 12:11:26 -0400

    corrections to existing

--- examples/OSCtest.sh
+++ examples/OSCtest.sh
@@ -2,93 +2,100 @@
 
 # will create a node using the "default" plugin
 clear
-oscsend localhost 18032 /spatosc/core ss createSource sheefa
-echo CREATE SOURCE NODE SHEEFA
+oscsend localhost 18032 /spatosc/core ss createSource mySound
+echo CREATE SOURCE NODE mySound
 sleep 1
-oscsend localhost 18032 /spatosc/core/source/sheefa/uri s plugin://default
+oscsend localhost 18032 /spatosc/core/source/mySound/uri s plugin://default
 echo SET DEFAULT PLUGIN
 sleep 1
-oscsend localhost 18032 /spatosc/core sss connect sheefa myListener
-echo CONNECT TO LISTENER
+oscsend localhost 18032 /spatosc/core sss connect mySound ear
+echo CONNECT TO LISTENER ear
 sleep 1
-oscsend localhost 18032 "/spatosc/core/connection/sheefa->myListener/update" fffff -1 0 0 1 22050  # azi ele  gain del lpf
+oscsend localhost 18032 "/spatosc/core/connection/mySound->ear/update" fffff -1 0 0 1 22050  # azi ele  gain del lpf
 echo PAN LEFT
 sleep 2
-oscsend localhost 18032 "/spatosc/core/connection/sheefa->myListener/update" fffff 1 0 1 0 22050
+oscsend localhost 18032 "/spatosc/core/connection/mySound->ear/update" fffff 1 0 1 0 22050
 echo PAN RIGHT
 sleep 2
-oscsend localhost 18032 "/spatosc/core/connection/sheefa->myListener/update" fffff 0 0 2 0 1000
+oscsend localhost 18032 "/spatosc/core/connection/mySound->ear/update" fffff 0 0 2 0 1000
 echo GAIN = 2 LPF = 1000
 sleep 2
-oscsend localhost 18032 "/spatosc/core/connection/sheefa->myListener/update" fffff 0 0 -12 0 22050
+oscsend localhost 18032 "/spatosc/core/connection/mySound->ear/update" fffff 0 0 -12 0 22050
 echo GAIN = -12
 sleep 2
-oscsend localhost 18032 "/spatosc/core/connection/sheefa->myListener/update" fffff 0 0 1 0 22050
+oscsend localhost 18032 "/spatosc/core/connection/mySound->ear/update" fffff 0 0 1 0 22050
 echo GAIN = 1
 sleep 1
-oscsend localhost 18032 "/spatosc/core/connection/sheefa->myListener/spread" f 0
+oscsend localhost 18032 "/spatosc/core/connection/mySound->ear/spread" f 0
 echo SPREAD WIDE
 sleep 1
-oscsend localhost 18032 "/spatosc/core/connection/sheefa->myListener/spread" f 20
+oscsend localhost 18032 "/spatosc/core/connection/mySound->ear/spread" f 20
 echo SPREAD NARROW
 sleep 1
-oscsend localhost 18032 "/spatosc/core/connection/sheefa->myListener/spread" f 1
+oscsend localhost 18032 "/spatosc/core/connection/mySound->ear/spread" f 1
 echo SPREAD NORMAL
 sleep 1
-oscsend localhost 18032 /spatosc/core/source/sheefa/state f 0
+oscsend localhost 18032 /spatosc/core/source/mySound/state f 0
 echo STATE = 0
 sleep 1
-oscsend localhost 18032 /spatosc/core/source/sheefa/state f 1
+oscsend localhost 18032 /spatosc/core/source/mySound/state f 1
 echo STATE = 1
 sleep 1
-oscsend localhost 18032 /spatosc/core/source/sheefa/event sff note  60 1
+oscsend localhost 18032 /spatosc/core/source/mySound/event sff note  60 1
 echo SET NOTE VALUES: 60 1  : midiPitch  amp
 sleep .5
-oscsend localhost 18032 /spatosc/core/source/sheefa/event sf t_trigger 1
+oscsend localhost 18032 /spatosc/core/source/mySound/event sf t_trigger 1
 echo TRIGGER NOTE
 sleep 1
-oscsend localhost 18032 /spatosc/core/source/sheefa/event sff note 76 .5
+oscsend localhost 18032 /spatosc/core/source/mySound/event sff note 76 .5
 echo SET NOTE VALUES: 76 .5  :  midiPitch  amp
 sleep .5
-oscsend localhost 18032 /spatosc/core/source/sheefa/event sf t_trigger 1
+oscsend localhost 18032 /spatosc/core/source/mySound/event sf t_trigger 1
 echo TRIGGER NOTE
 sleep 2
-oscsend localhost 18032 /spatosc/core/source/sheefa/uri s plugin://pink
+oscsend localhost 18032 /spatosc/core/source/mySound/uri s plugin://pink
 echo SET URI to  plugin://pink
 sleep 3
-oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf sfreq 1000
+oscsend localhost 18032 /spatosc/core/source/mySound/prop sf sfreq 1000
 echo PROP:  sfreq = 1000
 sleep 1
-oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf sfreq 500
+oscsend localhost 18032 /spatosc/core/source/mySound/prop sf sfreq 500
 echo PROP:  sfreq = 500
 sleep 1
-oscsend localhost 18032 /spatosc/core/source/sheefa/uri s plugin://dust
+oscsend localhost 18032 /spatosc/core/source/mySound/uri s plugin://dust
 echo SET URI to  plugin://dust
 sleep 1
-oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf density1 20
+oscsend localhost 18032 /spatosc/core/source/mySound/prop sf density1 20
 echo PROP:  density1 = 20
 sleep 3
-oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf density2 400
+oscsend localhost 18032 /spatosc/core/source/mySound/prop sf density2 400
 echo PROP:  density2 = 400
 sleep 3
-oscsend localhost 18032 /spatosc/core ss deleteNode sheefa
+oscsend localhost 18032 /spatosc/core ss deleteNode mySound
 echo DELETE NODE SHEEFA
 sleep 0
 
 
 
-oscsend localhost 18032 /spatosc/core ss createSource sheefa
+oscsend localhost 18032 /spatosc/core ss createSource mySound
 sleep 1
-oscsend localhost 18032 /spatosc/core sss connect sheefa myListener
+oscsend localhost 18032 /spatosc/core sss connect mySound ear
 sleep 1
-oscsend localhost 18032 /spatosc/core/source/sheefa/uri s plugin://zkarpluck1
+oscsend localhost 18032 /spatosc/core/source/mySound/uri s plugin://zkarpluck1
 sleep 1
-oscsend localhost 18032 /spatosc/core/source/sheefa/event sf t_gate 1
+oscsend localhost 18032 /spatosc/core/source/mySound/event sf t_gate 1
 sleep 1
-oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf c3 1
+oscsend localhost 18032 /spatosc/core/source/mySound/prop sf c3 1
 sleep
-oscsend localhost 18032 /spatosc/core/source/sheefa/event sf t_gate 1
+oscsend localhost 18032 /spatosc/core/source/mySound/event sf t_gate 1
 sleep 1
-oscsend localhost 18032 /spatosc/core/source/sheefa/prop sf c3 80
+oscsend localhost 18032 /spatosc/core/source/mySound/prop sf c3 80
 sleep
-
+oscsend localhost 18032 /spatosc/core/source/mySound/prop sf amp .1
+sleep 1
+oscsend localhost 18032 /spatosc/core/source/mySound/event sf t_gate 1
+sleep 1
+sleep oscsend localhost 18032 /spatosc/core/source/mySound/prop sf amp 1
+sleep 1
+oscsend localhost 18032 /spatosc/core/source/mySound/event sf t_gate 1
+sleep 1
