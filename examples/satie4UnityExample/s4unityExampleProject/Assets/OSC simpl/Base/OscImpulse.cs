282d6620e3045356255ef5f86acb0220cf296696
Zack Konate <zz@bell.net>
2017-03-12 03:56:52 -0400

    adding oscSimple code

---
+++ examples/satie4UnityExample/s4unityExampleProject/Assets/OSC simpl/Base/OscImpulse.cs
@@ -0,0 +1,20 @@
+﻿/*
+	Created by Carl Emil Carlsen.
+	Copyright 2016 Sixth Sensor.
+	All rights reserved.
+	http://sixthsensor.dk
+*/
+
+
+/// <summary>
+/// Class representing the OSC 1.1 argument type Impulse.
+/// In OSC 1.0 this was called 'Infinitum'.
+/// </summary>
+
+public class OscImpulse
+{
+	public override string ToString()
+	{
+		return "Impulse";
+	}
+}
---
+++ examples/satie4UnityExample/s4unityExampleProject/Assets/OSC simpl/Base/OscReceiveMode.cs
@@ -0,0 +1,12 @@
+﻿/*
+	Created by Carl Emil Carlsen.
+	Copyright 2016 Sixth Sensor.
+	All rights reserved.
+	http://sixthsensor.dk
+*/
+
+/// <summary>
+/// Enum representing the mode of transmission for OscIn.
+/// Can either be UnicastBroadcast or UnicastBroadcastMulticast.
+/// </summary>
+public enum OscReceiveMode { UnicastBroadcast, UnicastBroadcastMulticast }
