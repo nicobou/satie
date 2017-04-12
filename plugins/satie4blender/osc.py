9860f91ae32bd4d42b02143a315f88026280094c
Michal Seta <djiamnot@gmail.com>
2017-04-12 01:01:53 -0400

    some grunt work for the new osc protocol

--- plugins/satie4blender/osc.py
+++ plugins/satie4blender/osc.py
@@ -13,12 +13,19 @@
 
 import bpy
 import liblo
+import os
 
 from . import json_handler
+from . import properties as props
 
 bpy.s4b_OSCserver = None
 bpy.s4b_OSCclient = None
 
+
+NODE_TYPES = ["source", "group", "process"]
+SCENE_URI = "/satie/scene"
+
+OSC_ADDRESS = libro.Address(props.destination, props.satie_port)
 
 def init_osc_server():
     bpy.s4b_OSCclient = liblo.Address("localhost", 18032)
@@ -63,3 +70,56 @@ def stop_osc_server():
 
 def osc_rcv_cb(context):
     bpy.s4b_OSCserver.recv(0.01)
+
+def scene_create_source(nodeName, synthdefName, schemaName='plugin', group='default'):
+    """Create an audio source
+    An audio source is generator that produces sound (as opposed to DSP plugin or effect)
+
+    signature scene_create_source (nodeName, synthdefName, <group>)
+
+    nodeName: string - name of the SATIE instance
+    synthdefName: string - name of the compiled synthdef
+    group: string - optional, 'default' by default
+    """
+    liblo.send(OSC_ADDRESS, SCENE_URI, "createSource", nodeName,  "plugin://"+synthdefName, group)
+
+def scene_create_effect(nodeName, synthdefName, schemaName='plugin', group='defaultFx', in_bus=0):
+    """Create an audio effect
+    An audio source is a DSP plugin that acts on the signal provided on its bus number
+
+    signature scene_create_effect(nodeName, synthdefName, schema, <group>, in_bus)
+
+    nodeName: string - name of the SATIE instance
+    synthdefName: string - name of the compiled synthdef
+    group: string - optional, 'default' by default
+    in_bus: int - bus number, default=0
+    """
+    liblo.send(OSC_ADDRESS, SCENE_URI, "createSource", nodeName, "effect://"+synthdefName, group, in_bus)
+
+def scene_create_group(nodeName, schema='plugin'):
+    """Create a group"""
+    liblo.send(OSC_ADDRESS, SCENE_URI, "createGroup", nodeName, "effect://")
+
+def scene_clear():
+    liblo.send(OSC_ADDRESS, SCENE_URI, "clear")
+
+def scene_set(key, value):
+    liblo.send(OSC_ADDRESS, os.path.join(SCENE_URI, "set"), key, value)
+
+def node_state(nodeType, nodeName, value):
+    uri = os.path.join("/satie", nodeType, "state")
+    liblo.send(OSC_ADDRESS, uri, nodeName, value)
+
+def node_event(nodeType, nodeName, eventName, *args):
+    uri = os.path.join("/satie", nodeType, "event")
+    liblo.send(OSC_ADDRESS, uri, nodeName, eventName, args)
+
+def node_set(nodeType, nodeName, *args):
+    """
+    nodeType: string - nodeType: source, group, process
+    nodeName: string - instance name
+    args: array - key, value pairs alternating
+    """
+    uri = os.path.join("/satie", nodeType, "set")
+    liblo.send(OSC_ADDRESS, uri, nodeName, args)
+    
