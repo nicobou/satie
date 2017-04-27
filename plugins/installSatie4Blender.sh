b2ae3db534ee189976029528ea1dc5aad7292748
Michal Seta <djiamnot@gmail.com>
2017-04-27 13:45:54 +0000

    Remove blender addon

--- plugins/installSatie4Blender.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd satie4blender
-version=`blender --version | egrep -o "Blender [0-9.]+" | egrep -o "[0-9.]+"`
-mkdir -p ~/.config/blender/$version/scripts/addons/satie4blender
-cp -v * ~/.config/blender/$version/scripts/addons/satie4blender
--- plugins/satie4blender/zipit.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd .. && zip -r -x *~ *# satie4blender/zipit.sh @ satie4blender.zip satie4blender/
